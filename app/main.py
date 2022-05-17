f8abe6a2db87a994833a6a6c0988a5759c518370
Arosh Segar <67275168+arosh-segar@users.noreply.github.com>
2022-05-18 00:10:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from typing import Optional
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def read_root():
